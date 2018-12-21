cc0be2b702270b1de82222411837b8e60c28f70c
funnygay <1316747593@qq.com>
2018-12-21 10:55:04 +0800

    界面改动

--- app/src/main/java/com/example/yanghan/gravity/ui/team/GroupMessageActivity.java
+++ app/src/main/java/com/example/yanghan/gravity/ui/team/GroupMessageActivity.java
@@ -145,4 +145,9 @@ public class GroupMessageActivity extends AppCompatActivity {
  private void FillTheBlank(){
         //TODO fill message work
  }
+
+
+
+
+
 }
--- app/src/main/java/com/example/yanghan/gravity/ui/team/GroupMessageChangeActivity.java
+++ app/src/main/java/com/example/yanghan/gravity/ui/team/GroupMessageChangeActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.widget.Toolbar;
 import studio.carbonylgroup.textfieldboxes.ExtendedEditText;
 import studio.carbonylgroup.textfieldboxes.TextFieldBoxes;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,6 +26,7 @@ public class GroupMessageChangeActivity extends AppCompatActivity {
     private ExtendedEditText name;
     private ExtendedEditText match;
     private ExtendedEditText profile;
+    private TextFieldBoxes matchconnect;
     private Drawer result = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +46,9 @@ public class GroupMessageChangeActivity extends AppCompatActivity {
 
         init();
 
+
+
+//信息修改确认点击事件
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -54,7 +60,6 @@ public class GroupMessageChangeActivity extends AppCompatActivity {
                 }
                 else{
                     GroupMessageActivity.groupname=name.getText().toString();
-                    GroupMessageActivity.associatedevent=match.getText().toString();
                     GroupMessageActivity.teamprofile=profile.getText().toString();
                     Intent intent=new Intent(GroupMessageChangeActivity.this,GroupMessageActivity.class);
                     startActivity(intent);
@@ -62,24 +67,46 @@ public class GroupMessageChangeActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+
+
+
+
+//取消关联赛事点击事件
+        matchconnect.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if( GroupMessageActivity.associatedevent!="暂无赛事关联")
+                showNormalDialog();
+
+            }
+        });
+
     }
 
 
-
+//初始化控件
    private void init(){
-       button=(Button )findViewById(R.id.button);
+       button=(Button)findViewById(R.id.button);
        name=(ExtendedEditText)findViewById(R.id.edit_note);
        match=(ExtendedEditText)findViewById(R.id.match_edit_note);
        profile=(ExtendedEditText)findViewById(R.id.profile_note);
+       matchconnect=(TextFieldBoxes)findViewById(R.id.match_edit_box);
        name.setText(GroupMessageActivity.groupname);
        match.setText(GroupMessageActivity.associatedevent);
        profile.setText(GroupMessageActivity.teamprofile);
     }
 
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
     }
+
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -102,4 +129,45 @@ public class GroupMessageChangeActivity extends AppCompatActivity {
         }
         // this.finish();
     }
+
+
+
+//确认弹窗
+    private void showNormalDialog(){
+        /* @setIcon 设置对话框图标
+         * @setTitle 设置对话框标题
+         * @setMessage 设置对话框消息提示
+         * setXXX方法返回Dialog对象，因此可以链式设置属性
+         */
+        final AlertDialog.Builder normalDialog =
+                new AlertDialog.Builder(GroupMessageChangeActivity.this);
+        normalDialog.setTitle("是否取消关联该赛事？");
+        normalDialog.setPositiveButton("确定",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //...To-do remove the match
+                        GroupMessageActivity.associatedevent="暂无赛事关联";
+                        refresh();
+                    }
+                });
+        normalDialog.setNegativeButton("关闭",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //...To-do nothing
+                    }
+                });
+        // 显示
+        normalDialog.show();
+    }
+
+
+//刷新界面
+    private void refresh() {
+        finish();
+        Intent intent = new Intent(GroupMessageChangeActivity.this, GroupMessageChangeActivity.class);
+        startActivity(intent);
+    }
+
 }
