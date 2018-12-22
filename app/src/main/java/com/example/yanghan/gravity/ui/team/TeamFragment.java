082cf86cd651a708a752801af677948df33127d2
funnygay <1316747593@qq.com>
2018-12-22 18:10:24 +0800

    。。。。

--- app/src/main/java/com/example/yanghan/gravity/ui/team/TeamFragment.java
+++ app/src/main/java/com/example/yanghan/gravity/ui/team/TeamFragment.java
@@ -8,9 +8,11 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,17 +22,22 @@ import android.widget.SimpleAdapter;
 
 import com.example.yanghan.gravity.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.wuxiaolong.pullloadmorerecyclerview.PullLoadMoreRecyclerView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-public class TeamFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+public class TeamFragment extends Fragment implements PullLoadMoreRecyclerView.PullLoadMoreListener {
 
-    private SwipeRefreshLayout swipeRefreshLayout;
-    private  SimpleAdapter adapter;
-    //private RefreshListAdapter adapter;
-    private LoadMoreListView loadMoreListView;
+
+
+    private PullLoadMoreRecyclerView mPullLoadMoreRecyclerView;
+    private int mCount = 1;
+    private RecyclerViewAdapter mRecyclerViewAdapter;
+    private RecyclerView mRecyclerView;
+
+
     private View v;
     List<Map<String, Object>> listitem = new ArrayList<Map<String, Object>>(); //存储数据的数组列表
 
@@ -47,42 +54,11 @@ public class TeamFragment extends Fragment implements SwipeRefreshLayout.OnRefre
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-         v = inflater.inflate(R.layout.fragment_team, container, false);
+        v = inflater.inflate(R.layout.fragment_team, container, false);
         InitView();
 
 
-        //loadMoreListView = (LoadMoreListView) v.findViewById(R.id.google_and_loadmore_refresh_listview);
-        //loadMoreListView.setOnRefreshListener(this);
 
-        swipeRefreshLayout.setOnRefreshListener(this);
-
-        //getData(); //query data from a database
-
-        adapter = new SimpleAdapter(getActivity()
-                , listitem
-                , R.layout.items_group
-                , new String[]{"group_logo","group_name"}
-                , new int[]{R.id.group_logo, R.id.group_name});
-        // 第一个参数是上下文对象
-        // 第二个是listitem
-        // 第三个是指定每个列表项的布局文件
-        // 第四个是指定Map对象中定义的两个键（这里通过字符串数组来指定）
-        // 第五个是用于指定在布局文件中定义的id（也是用数组来指定）
-
-        ListView listView = (ListView) v.findViewById(R.id.team_group);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {//设置监听器
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                //  Toast.makeText(getActivity(), map.get("group_name").toString(), Toast.LENGTH_LONG).show();
-
-                Intent intent=new Intent(getActivity(),GroupMessageActivity.class);
-                startActivity(intent);
-
-            }
-        });
         FloatingActionButton fab = ( FloatingActionButton )v.findViewById(R.id.floatingActionButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -101,7 +77,36 @@ public class TeamFragment extends Fragment implements SwipeRefreshLayout.OnRefre
         return v;
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mPullLoadMoreRecyclerView = (PullLoadMoreRecyclerView) view.findViewById(R.id.pullLoadMoreRecyclerView);
+        //获取mRecyclerView对象
+        mRecyclerView = mPullLoadMoreRecyclerView.getRecyclerView();
+        //代码设置scrollbar无效？未解决！
+        mRecyclerView.setVerticalScrollBarEnabled(true);
+        //设置下拉刷新是否可见
+        //mPullLoadMoreRecyclerView.setRefreshing(true);
+        //设置是否可以下拉刷新
+        //mPullLoadMoreRecyclerView.setPullRefreshEnable(true);
+        //设置是否可以上拉刷新
+        //mPullLoadMoreRecyclerView.setPushRefreshEnable(false);
+        //显示下拉刷新
+        mPullLoadMoreRecyclerView.setRefreshing(true);
+        //设置上拉刷新文字
+        mPullLoadMoreRecyclerView.setFooterViewText("loading");
+        //设置上拉刷新文字颜色
+        //mPullLoadMoreRecyclerView.setFooterViewTextColor(R.color.white);
+        //设置加载更多背景色
+        //mPullLoadMoreRecyclerView.setFooterViewBackgroundColor(R.color.colorBackground);
+        mPullLoadMoreRecyclerView.setLinearLayout();
 
+        mPullLoadMoreRecyclerView.setOnPullLoadMoreListener(this);
+        mRecyclerViewAdapter = new RecyclerViewAdapter(getActivity());
+        mPullLoadMoreRecyclerView.setAdapter(mRecyclerViewAdapter);
+        //getData();
+
+    }
 
 
     @Override
@@ -112,7 +117,7 @@ public class TeamFragment extends Fragment implements SwipeRefreshLayout.OnRefre
     }
 
     private void InitView(){
-        swipeRefreshLayout = (SwipeRefreshLayout)v.findViewById(R.id.swipe_refresh_layout);
+
         //写死的数据，用于测试
         //插入队伍头像图片
         imagelist.add(R.mipmap.ic_launcher);
@@ -132,59 +137,64 @@ public class TeamFragment extends Fragment implements SwipeRefreshLayout.OnRefre
     }
 
 
+    private void getData() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mRecyclerViewAdapter.addAllData(setList());
+                        Log.e("number=","here!");
+
+                       mPullLoadMoreRecyclerView.setPullLoadMoreCompleted();
+                        Log.e("number=","place2");
+                    }
+                });
+
+            }
+        }, 1000);
+
+    }
+
+    public void clearData() {
+        mRecyclerViewAdapter.clearData();
+        mRecyclerViewAdapter.notifyDataSetChanged();
+    }
 
 
+    private List<Map<String, Object>> setList() {
+        List<Map<String, Object>> dataList = new ArrayList<>();
+        int start = 6 * (mCount - 1);
+        for (int i = start; i < 6 * mCount; i++) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("group_logo", imagelist.get(i));
+            map.put("group_name", groupnamelist.get(i));
+            dataList.add(map);
+        }
+        return dataList;
 
+    }
 
     @Override
     public void onRefresh() {
-        //因为本例中没有从网络获取数据，因此这里使用Handler演示4秒延迟来从服务器获取数据的延迟现象，以便于大家
-        // 能够看到listView正在刷新的状态。在现实使用时只需要使用run（）{}方法中的代码就行了。
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                //获取最新的list数据
-                setRefreshData();
-                //通知界面显示，
-                adapter.notifyDataSetChanged();
-                // 通知listview刷新数据完毕,让listview停止刷新
-                swipeRefreshLayout.setRefreshing(false);
-            }
-        }, 600);
+        Log.e("wxl", "onRefresh");
+        setRefresh();
+       // getData();
+    }
+
+    @Override
+    public void onLoadMore() {
+        Log.e("wxl", "onLoadMore");
+        mCount = mCount + 1;
+      //  getData();
+    }
+
+    private void setRefresh() {
+        mRecyclerViewAdapter.clearData();
+        mCount = 1;
     }
 
 
-//    @Override
-//    public void onLoadingMore() {
-//
-//        //因为本例中没有从网络获取数据，因此这里使用Handler演示4秒延迟来从服务器获取数据的延迟现象，以便于大家
-//        // 能够看到listView正在刷新的状态。大家在现实使用时只需要使用run（）{}方法中的代码就行了。
-//        Handler handler = new Handler();
-//        handler.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                //获取最新的list数据
-//                setRefreshData();
-//                //通知界面显示，
-//                adapter.notifyDataSetChanged();
-//                // 通知listview刷新数据完毕,让listview停止刷新,取消加载跟多
-//                loadMoreListView.loadMoreComplete();
-//            }
-//        }, 6000);
-//    }
-
-
-    private void setRefreshData() {
-
-        //这里只是模拟3个列表项数据，在现实开发中，listview中的数据都是从服务器获取的。
-//        for (int i = 0; i < 3; i++) {
-//            ListViewItem item = new ListViewItem();
-//            item.setUserImg(R.mipmap.ic_launcher);
-//            item.setUserName("seven" + i);
-//            item.setUserComment("这是一个下拉刷新，上拉加载更多的ListView");
-//            items.add(item);
-//        }
-    }
 
 }
