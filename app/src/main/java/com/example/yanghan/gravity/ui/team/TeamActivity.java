2402f40f0b963d4387e62c407132939fdcb94569
funnygay <1316747593@qq.com>
2018-12-23 12:00:30 +0800

    加了个toolbar

--- app/src/main/java/com/example/yanghan/gravity/ui/team/TeamActivity.java
+++ app/src/main/java/com/example/yanghan/gravity/ui/team/TeamActivity.java
@@ -4,24 +4,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import com.example.yanghan.gravity.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
 
 import java.util.ArrayList;
 
-public class TeamActivity extends Activity implements SwipeRefreshLayout.OnRefreshListener, LoadMoreListView.OnRefreshListener{
+public class TeamActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener, LoadMoreListView.OnRefreshListener{
     private SwipeRefreshLayout swipeRefreshLayout;
     private LoadMoreListView loadMoreListView;
     //对象数据集合
     private ArrayList<ListViewItem> items;
     //listview的数据加载器adapter
     private RefreshListAdapter adapter;
+    private Drawer result = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team);
+
+        Toolbar team_toolbar = (Toolbar) findViewById(R.id.team_toolbar);
+        setSupportActionBar(team_toolbar);
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withSavedInstance(savedInstanceState)
+                .withFullscreen(true)
+                .build();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(false);
+
         initView();
         initEvent();
         initData();
@@ -142,5 +164,32 @@ public class TeamActivity extends Activity implements SwipeRefreshLayout.OnRefre
         }, 6000);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.menu_grouptoolbar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
 
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        //handle the back press :D close the drawer first and if the drawer is closed close the activity
+        if (result != null && result.isDrawerOpen()) {
+            result.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
+        // this.finish();
+    }
 }
